ea5df682740bbed8ccddfb8a4140787e03df2815
loren <gutierrezloren08@gmail.com>
2024-10-10 20:29:28 -0500

    primer commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,65 @@
 import './bootstrap';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
+
+document.addEventListener('DOMContentLoaded', function() {
+    const calendarEl = document.getElementById('calendar');
+    const dateInput = document.getElementById('datesemana');
+    const timeInput = document.getElementById('horaevento'); 
+
+    
+
+    const calendar = new Calendar(calendarEl, {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        events: '/eventos',
+
+        dateClick: function(info) {
+            const dateStr = info.dateStr; // Formato yyyy-mm-dd
+
+            // Obtener la fecha actual
+            const now = new Date();
+            const selectedDateTime = new Date(dateStr + 'T00:07:00'); 
+
+            // Validar si la fecha seleccionada es anterior a la fecha actual
+            if (selectedDateTime < now) {
+                alert('No se puede agregar eventos en fechas pasadas.');
+                return;
+            }
+
+            const title = prompt('Ingrese el título del evento:');
+
+            // Validar si se ingresó un título
+            if (title) {
+                const eventData = {
+                    title: title,
+                    start: dateStr,
+                    allDay: false
+                };
+
+                // Agregar evento al calendario
+                calendar.addEvent(eventData);
+
+                const selectedTime = timeInput.value || '07:00';
+
+                const fullDateTime = dateStr + 'T' + selectedTime;
+                
+                dateInput.value = fullDateTime; 
+
+                console.log("Fecha y hora seleccionadas:", fullDateTime);
+
+            } else {
+                alert('Título del evento es obligatorio.');
+            }
+        },
+    });
+
+    calendar.render();
+});
 
 Alpine.start();
